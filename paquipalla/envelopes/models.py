eb0db341880acce087fae05e82f3a433ca469b4e
rozeoner <sergiovargaspuy@gmail.com>
2019-02-26 12:38:24 +0100

    models updated

--- paquipalla/envelopes/models.py
+++ paquipalla/envelopes/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
 class Company(models.Model):
-        name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 class Envelope(models.Model):
     amount = models.IntegerField()
-    motivation = models.CharField(max_length=100)
-    company = models.ForeingKey(Company, on_delete=models.CASCADE)
+    motive = models.CharField(max_length=100)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.company.name+" - "+self.motive

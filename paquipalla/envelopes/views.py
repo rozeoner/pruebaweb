2a592627559ba541abcd80845b0be32d3c312de9
rozeoner <sergiovargaspuy@gmail.com>
2019-02-26 12:47:08 +0100

    urls and views updated

--- paquipalla/envelopes/views.py
+++ paquipalla/envelopes/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
 # Create your views here.
+def mainpage(request):
+    return HttpResponse("<html><body>HELLO</body></html>")
